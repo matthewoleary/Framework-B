2fbbbcb0ed082b5057b70a40f854745364a15fc9
Matthew O'Leary <moleary@zendesk.com>
2020-08-04 21:58:10 +0100

    Adding .framework

---
+++ Framework-B/Build/Debug-iphonesimulator/Framework_B.framework/Headers/Framework_B.h
@@ -0,0 +1,19 @@
+//
+//  Framework_B.h
+//  Framework-B
+//
+//  Created by Matthew O'Leary on 04/08/2020.
+//  Copyright © 2020 Matthew O'Leary. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Framework_B.
+FOUNDATION_EXPORT double Framework_BVersionNumber;
+
+//! Project version string for Framework_B.
+FOUNDATION_EXPORT const unsigned char Framework_BVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Framework_B/PublicHeader.h>
+
+
