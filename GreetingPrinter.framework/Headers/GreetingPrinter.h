4e3c009a0de4bbf4ed9565954144f72da235a57d
Matthew O'Leary <moleary@zendesk.com>
2020-08-05 01:03:26 +0100

    Adding Greetingprinter

---
+++ GreetingPrinter.framework/Headers/GreetingPrinter.h
@@ -0,0 +1,19 @@
+//
+//  GreetingPrinter.h
+//  GreetingPrinter
+//
+//  Created by Matthew O'Leary on 05/08/2020.
+//  Copyright © 2020 Matthew O'Leary. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GreetingPrinter.
+FOUNDATION_EXPORT double GreetingPrinterVersionNumber;
+
+//! Project version string for GreetingPrinter.
+FOUNDATION_EXPORT const unsigned char GreetingPrinterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GreetingPrinter/PublicHeader.h>
+
+
